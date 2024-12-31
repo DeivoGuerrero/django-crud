37b113960dcbd9f6ae85614972aac2d1dace2439
David Guerrero <deivo.guerrero@gmail.com>
2024-12-31 12:45:19 -0500

    Funcionalidad finalizada

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,19 +1,7 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Task
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    description = forms.CharField(widget=forms.Textarea)
-    completed = forms.BooleanField(required=False)
-    
-    def __init__(self, *args, **kwargs):
-        instance = kwargs.pop('instance', None)
-        super(TaskForm, self).__init__(*args, **kwargs)
-        if instance:
-            self.fields['title'].initial = instance.title
-            self.fields['description'].initial = instance.description
-            self.fields['completed'].initial = instance.completed
-        self.fields['title'].widget.attrs.update({'class': 'form-control'})
-        self.fields['description'].widget.attrs.update({'class': 'form-control'})
-        self.fields['completed'].widget.attrs.update({'class': 'form-check-input'})
-
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'completed']
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
@@ -9,6 +10,20 @@ from .forms import TaskForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': AuthenticationForm()})
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('tasks')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+            return render(request, 'login.html', {'form': AuthenticationForm()})
 
 def singup(request):
     if request.method == 'GET':
@@ -28,6 +43,7 @@ def singup(request):
             messages.error(request, form.errors)
             return render(request, 'singup.html', {'form': form})
         
+@login_required
 def tasks(request):
     if request.method == 'GET':
         tasks = Task.objects.filter(user=request.user)
@@ -43,41 +59,31 @@ def tasks(request):
             messages.error(request, form.errors)
             return render(request, 'tasks.html', {'form': form})
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('home')
 
-def login_view(request):
-    if request.method == 'GET':
-        return render(request, 'login.html', {'form': AuthenticationForm()})
-    elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('tasks')
-        else:
-            messages.error(request, 'Usuario o contraseña incorrectos')
-            return render(request, 'login.html', {'form': AuthenticationForm()})
-        
+@login_required
 def edit_task(request, id):
-    if request.method == 'GET':
-        task = Task.objects.get(id=id)
-        form = TaskForm(instance=task)
-        return render(request, 'edit_task.html', {'form': form})
-    elif request.method == 'POST':
-        task = Task.objects.get(id=id)
-        form = TaskForm(request.POST)
+    task = Task.objects.get(id=id)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Tarea actualizada correctamente')
+            messages.success(request, 'Tarea editada correctamente')
             return redirect('tasks')
         else:
             messages.error(request, form.errors)
             return render(request, 'edit_task.html', {'form': form})
-    
-    
+    context = {
+        'form': form,
+        'task': task
+    }
+    return render(request, 'edit_task.html', context)
+
+@login_required
 def delete_task(request, id):
     task = Task.objects.get(id=id)
     task.delete()
